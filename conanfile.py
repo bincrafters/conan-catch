01350c6f67cf3f07cce87434410e130328636856
Jerry W <jerrywiltse@gmail.com>
2018-01-03 09:39:16 -0500

    refresh all and add ci

--- conanfile.py
+++ conanfile.py
@@ -2,22 +2,36 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, tools
+import os
 
 
 class CatchConan(ConanFile):
-    name = "Catch"
+    name = "catch"
     version = "2.0.1"
-    generators = "cmake"
-    url = "https://github.com/bincrafters/conan-catch"
-    license = "www.boost.org/LICENSE_1_0.txt"
     description = "A modern, C++-native, header-only, framework for unit-tests, TDD and BDD"
+    url = "https://github.com/bincrafters/conan-catch"
+    license = "BSL-1.0"
+    exports = ["LICENSE.md"]
+    source_subfolder = "source_subfolder"
+    header_name = "catch.hpp"
 
     def source(self):
-        header_name = "catch.hpp"
-        tools.download("https://github.com/catchorg/Catch2/releases/download/v%s/%s" % (self.version, header_name), header_name)
+        source_url = "https://github.com/catchorg/Catch2"
+        
+        tools.download(
+            "{0}/releases/download/v{1}/{2}".format(source_url, self.version, self.header_name),
+            self.header_name
+        )
+        
+        # All this to get the LICENSE.txt
+        extracted_dir = "Catch2-" + self.version
+        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        os.rename(extracted_dir, self.source_subfolder)
+        
         
     def package(self):
-        self.copy(pattern="catch.hpp", dst="include")
+        self.copy(pattern="LICENSE.txt", src=self.source_subfolder)
+        self.copy(pattern=self.header_name, dst="include")
 
     def package_id(self):
         self.info.header_only()
